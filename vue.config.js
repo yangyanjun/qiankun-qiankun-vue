39f55eb0be31c76fa74390f7e660daed707669bb
yangyanjun <375663014@qq.com>
2023-06-18 23:47:10 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: '10000',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: 'vueApp',
+      libraryTarget: 'umd',
+    }
+  }
 })
